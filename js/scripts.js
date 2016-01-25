aae2f6850a59057cf69a60be506b2a412ec36f57
Daren Schaad and Taylor Pokoj <pokojt@gmail.com>
2016-01-25 09:12:48 -0800

    Add user interface code, appending results and adding div column for clicking on each contact to display info on right.

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,32 @@ function Contact(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
-}
+};
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-}
+};
 
 
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
+
     var inputtedFirstName = $("input#inputtedFirstName").val();
     var inputtedLastName = $("input#inputtedLastName").val();
     var newContact = new Contact (inputtedFirstName, inputtedLastName);
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
     $("input#inputtedFirstName").val("");
     $("input#inputtedLastName").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
   });
+
 });
