3c17ba1f371110ed853001a6abde5e5f3e14889d
Simon Temple and Taylor Pokoj <pokojt@gmail.com>
2016-01-26 10:03:52 -0800

    Added specs and functionality for adding multiple addresses to each contact.

--- spec/specs.js
+++ spec/specs.js
@@ -3,12 +3,26 @@ describe ('Contact', function(){
     var newContact = new Contact("Daren", "Schaad");
     expect(newContact.firstName).to.equal("Daren");
     expect(newContact.lastName).to.equal("Schaad");
-    expect(newContact.addresses).to.eql([]);
+    expect(newContact.addresses).to.equal([])
   });
 
   it("adds the fullName method to a contact", function () {
     var newContact = new Contact("Taylor", "Pokoj");
     expect(newContact.fullName()).to.equal("Taylor Pokoj");
+  });
+
+});
+describe ('Address', function(){
+  it("creates a new address with the given properties", function() {
+    var newAddress = new Address("123 Main", "Portland", "Oregon");
+    expect(newAddress.street).to.equal("123 Main");
+    expect(newAddress.city).to.equal("Portland");
+    expect(newAddress.state).to.eql("Oregon");
+  });
+
+  it("adds the fullAddress method to a contact", function () {
+    var newAddress = new Address("123 Main", "Portland", "Oregon");
+    expect(newAddress.fullAddress()).to.equal("123 Main, Portland, Oregon");
   });
 
 });
