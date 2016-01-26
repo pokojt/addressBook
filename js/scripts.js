875697ea61d16b36fe0a2cb2829bcfafecc9ba10
Simon Temple and Taylor Pokoj <pokojt@gmail.com>
2016-01-26 11:59:09 -0800

    Try to add label to addresses but fail miserably

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,13 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
-function Address(street, city, state) {
+function Address(street, city, state, addressType, email) {
   this.street = street;
   this.city = city;
   this.state = state;
+  this.addressType = addressType;
+  this.email = email;
+
 };
 
 Address.prototype.fullAddress = function() {
@@ -20,9 +23,35 @@ Address.prototype.fullAddress = function() {
 
 
 $(document).ready(function() {
-  $("#add-address").click(function() {
+  $("#add-address-home").click(function() {
+    var addressType = "Home"
     $("#new-addresses").append('<div id="new-addresses2">' +
                                 '<div class="new-address">' +
+                                '<h3 class="addressHeader">Home</h3>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                                '</div>' +
+                               '</div>');
+
+
+
+
+  });
+  $("#add-address-work").click(function() {
+    var addressType = "Work"
+    $("#new-addresses").append('<div id="new-addresses2">' +
+                                '<div class="new-address">' +
+                                '<h3 class="addressHeader">Work</h3>' +
                                  '<div class="form-group">' +
                                    '<label for="new-street">Street</label>' +
                                    '<input type="text" class="form-control new-street">' +
@@ -40,6 +69,46 @@ $(document).ready(function() {
 
 
   });
+  $("#add-address-school").click(function() {
+    var addressType = "School"
+    $("#new-addresses").append('<div id="new-addresses2">' +
+                                '<div class="new-address">' +
+                                '<h3 class="addressHeader">School</h3>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                                '</div>' +
+                               '</div>');
+
+
+
+  });
+  $("#add-address-email").click(function() {
+    var addressType = "Email"
+    $("#new-addresses").append('<div id="new-addresses2">' +
+                                '<div class="new-address">' +
+                                '<h3 class="addressHeader">Email</h3>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Email Address</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 
+                                '</div>' +
+                               '</div>');
+
+
+
+  });
+
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -52,9 +121,17 @@ $(document).ready(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      var addressType = $(this).find(".addressHeader").val();
+
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, addressType);
       newContact.addresses.push(newAddress);
     });
+
+    $("div.dropdown-menu").click(function() {
+      var inputedAddress = $(".add-address-home").val();
+      var inputedAddress = $(".add-address-work").val();
+      var inputedAddress = $(".add-address-school").val();
+    })
 
     $('#new-addresses2:last-child', this).remove();
 
@@ -74,7 +151,7 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        $("ul#addresses").append(address.addressType + "<li>" + address.fullAddress() + "</li>");
       });
     });
   });
